03913e6a010365caca3e81b1f203339350dc195b
Abejithp <abejithp@hotmail.com>
2024-11-08 15:50:05 -0500

    added functionality to find broken links

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from google.oauth2 import service_account
 from dotenv import load_dotenv
 import requests
 import csv
+from bs4 import BeautifulSoup
+import time
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,6 +18,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = "credentials.json"  # Path to your service account file
 SPREADSHEET_ID = os.getenv("SHEET_ID")  # Replace with your spreadsheet ID
 SHEET_NAME = 'Sheet1'  # Replace with your sheet name
+MAX_LIMIT = 100000
 
 # Authenticate and build the Google Sheets service
 creds = service_account.Credentials.from_service_account_file(
@@ -132,6 +135,7 @@ def write_results(index, url, score, category):
 # Function to parse sitemap.xml and extract URLs
 def parse_xml(limit):
     page_audits = []
+    sub_xml = []
     current_limit = 0
 
     for url in audits:
@@ -142,14 +146,40 @@ def parse_xml(limit):
 
             for sub_url in root.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc"):
                 if '?' in sub_url.text:
-                    continue
+                    sub_xml.append(sub_url.text)
+                    continue   
+
                 if current_limit == limit:
                     current_limit = 0
                     break
-                current_limit += 1
+    
                 page_audits.append(sub_url.text)
+                current_limit += 1
+                
         except requests.exceptions.RequestException as e:
             print(e)
+
+        current_limit = 0
+
+
+    for url in sub_xml:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            root = ET.fromstring(response.content)
+
+            for sub_url in root.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc"):
+                if current_limit == limit:
+                    current_limit = 0
+                    break
+
+                page_audits.append(sub_url.text)
+                current_limit += 1
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+        current_limit = 0
 
     return page_audits
 
@@ -180,11 +210,134 @@ def delete_reports():
 
     print("All reports have been deleted")
 
+def write_broken(url, data, index):
+
+    sheet = service.spreadsheets()
+
+    sheet.values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range=f"Broken Links!A{index}:C{index}",
+        valueInputOption="USER_ENTERED",
+        body={"values": [[url, data[1], data[0]]]}
+    ).execute()
+
+
+def audit_links():
+
+    sheet = service.spreadsheets()
+    try:
+        sheet.values().get(spreadsheetId=SPREADSHEET_ID, range="Broken Links!A1").execute()
+        # Clear the existing data in the sheet:
+        sheet.values().clear(
+            spreadsheetId=SPREADSHEET_ID,
+            range="Broken Links!A1:Z1000"
+        ).execute()
+
+    except:
+        sheet.batchUpdate(
+            spreadsheetId=SPREADSHEET_ID,
+            body={
+                "requests": [
+                    {
+                        "addSheet": {
+                            "properties": {
+                                "title": "Broken Links"
+                            }
+                        }
+                    }
+                ]
+            }
+        ).execute()
+
+    #write headers
+    sheet.values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range="Broken Links!A1:C1",
+        valueInputOption="USER_ENTERED",
+        body={"values": [["Base URL", "URL Text", "Broken URL"]]}
+    ).execute()
+
+    print("Parsing links from site map")
+
+    links = parse_xml(20)
+    audit = {}
+
+    for link in links:
+        audit[link] = []
+
+    try:
+
+        print("Finding search results pages")
+
+        for link in links:
+            response = requests.get(link)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'html.parser')  
+
+            base_url = link.split(".ca")[0] + ".ca"
+            
+            for a in soup.find_all('a', href=True):
+                if('search-results' in a['href']):
+                    if("http" in a['href']):
+                        # check if the link is already in the audit
+                        if a['href'] not in audit[link]:
+                            audit[link].append(((a['href']), a.text))
+                    else:
+                        if (base_url + a['href'].replace(" ", "%20")) not in audit[link]:
+                            audit[link].append(((base_url +  a['href'].replace(" ", "%20")), a.text))
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+    broken = {}
+    index = 2
+
+    for key in audit:
+        broken[key] = []
+
+    print("Auditing search results pages")
+
+    for key in audit:
+        urls = audit[key]
+
+        for data in urls:
+            url = data[0]
+            print("Checking: " + url)
+
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                for p in soup.find_all('p'):
+                    if('search returned no results' in p.text):
+                        write_broken(key, data, index)
+                        broken[key].append(data)
+                        index += 1
+            
+            except requests.exceptions.RequestException as e:
+                print(e)
+
+            # add a delay to avoid getting blocked
+            time.sleep(1)
+
+    #write broken into a csv file
+    open('broken_links.csv', 'w').close()
+    with open('broken_links.csv', mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Base URL", "URL Text", "Broken URL"])
+        for key, value in broken.items():
+            for v in value:
+                writer.writerow([key, v[1], v[0]])
+
+    print("Audit complete")
+
 # Main function to run the script
 def main():
     print("1. Run audit only on routes")
     print("2. Run audit on sitemap.xml")
     print("3. Delete all reports")
+    print("4. Audit broken links")
     options = int(input("Enter your choice: "))
 
     if options == 1:
@@ -194,6 +347,9 @@ def main():
         pages = parse_xml(limit)
     elif options == 3:
         delete_reports()
+        return
+    elif options == 4:
+        audit_links()
         return
     else:
         print("Invalid choice")
